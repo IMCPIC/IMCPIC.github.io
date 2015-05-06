d76fd5ac51b977774c253009e8de9b8aac88c5a3
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-06 10:15:23 -0500

    Starting to enter blog posts

---
+++ assets/js/blogger.js
@@ -0,0 +1,16 @@
+$(document).ready(getposts);
+function getposts(){
+    $.getJSON('/entries.json', function(data) {
+		$.each(data.posts, function(key, val) {
+                var output="<ul>";
+				for (var i in data.entries) {
+				    output+="<li>" + data.entries[i].date +"</li>";
+				    output+="<li>" + data.entries[i].title +"</li>";
+				    output+="<li>" + data.entries[i].text +"</li>";
+				}
+
+				output+="</ul>";
+				$("#blog").html(output);
+      	}
+    }
+}
