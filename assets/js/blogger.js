dfbb76824290085fb01f6b6928f97edf19894cb4
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-06 10:24:44 -0500

    4

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,12 +2,10 @@ $(document).ready(getposts);
 function getposts(){
     $.getJSON('entries.json', function(data) {
 		$.each(data.posts, function(key, val) {
-                var output="<ul>";
-				/*for (var i in data.entries) {
-				    output+="<li>" + data.entries[i].date +"</li>";
-				    output+="<li>" + data.entries[i].title +"</li>";
-				    output+="<li>" + data.entries[i].text +"</li>";
-				}*/
+                var output="<ul id='"+key+"'>";
+				    output+="<li>" + val.date +"</li>";
+				    output+="<li>" + val.title +"</li>";
+				    output+="<li>" + val.text +"</li>";
 
 				output+="</ul>";
 				$("#blog").html(output);
