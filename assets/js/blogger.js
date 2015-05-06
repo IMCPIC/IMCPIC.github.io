bacadca91851eda519a44c15d9231e0377bc18fe
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-06 10:20:32 -0500

    3

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -3,11 +3,11 @@ function getposts(){
     $.getJSON('entries.json', function(data) {
 		$.each(data.posts, function(key, val) {
                 var output="<ul>";
-				for (var i in data.entries) {
+				/*for (var i in data.entries) {
 				    output+="<li>" + data.entries[i].date +"</li>";
 				    output+="<li>" + data.entries[i].title +"</li>";
 				    output+="<li>" + data.entries[i].text +"</li>";
-				}
+				}*/
 
 				output+="</ul>";
 				$("#blog").html(output);
