b062f6ba8f77c3274ef856e6054d79d7bbea8115
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-06 11:58:23 -0500

    2

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,13 +2,13 @@ $(document).ready(getposts);
 function getposts(){
     $.getJSON('entries.json', function(data) {
 		$.each(data.entries, function(key, val) {
-                var output="<ul id='key'>";
+                var output="<ul id='"+key+"'>";
 				    output+="<p class='muted' style='float:right;'>" + val.date +"</p>";
 				    output+="<h5>" + val.title +"</h5>";
 				    output+="<p>" + val.text +"</p>";
 					output+="</ul>";
 
-				$("#key").html(output);
+				$.each("#key").html(output);
       	});
     });
 }
