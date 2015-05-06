083ec05bb2ed61803fc811c1a4de735541dbcdfc
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-06 11:57:18 -0500

    1

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,7 +2,7 @@ $(document).ready(getposts);
 function getposts(){
     $.getJSON('entries.json', function(data) {
 		$.each(data.entries, function(key, val) {
-                var output="<ul id='"+key+"'>";
+                var output="<ul id='key'>";
 				    output+="<p class='muted' style='float:right;'>" + val.date +"</p>";
 				    output+="<h5>" + val.title +"</h5>";
 				    output+="<p>" + val.text +"</p>";
