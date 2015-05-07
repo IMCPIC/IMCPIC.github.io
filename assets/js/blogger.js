96b10421208129a5e7c1e1f480c5f8f0e0673067
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-07 11:58:55 -0500

    modifying search titles

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -18,19 +18,18 @@ function getposts(){
 
 
 $(window).load(function(){
-        $('#search').keyup(function(){
-            var searchField = $('#search').val();
+        $('#search2').keyup(function(){
+            var searchField = $('#search2').val();
             var regex = new RegExp(searchField, "i");
             var output = '<div class="row">';
             var count = 1;
             $.getJSON('entries.json', function(data) {
               $.each(data, function(key, val){
-                if ((val.name.search(regex) != -1) || (val.location.search(regex) != -1)) {
+                if ((val.title.search(regex) != -1) || (val.text.search(regex) != -1)) {
                   output += '<div class="col-md-6 well">';
-                  output += '<div class="col-md-3"><img class="img-responsive" src="'+val.avatar+'" alt="'+ val.name +'" /></div>';
                   output += '<div class="col-md-7">';
-                  output += '<h5>' + val.name + '</h5>';
-                  output += '<p>' + val.location + '</p>'
+                  output += '<h5>' + val.title + '</h5>';
+                  output += '<p>' + val.text + '</p>'
                   output += '</div>';
                   output += '</div>';
                   if(count%2 == 0){
