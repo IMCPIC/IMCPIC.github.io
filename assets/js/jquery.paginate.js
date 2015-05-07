a445b6254ddaea8a4df15708dc76d79d0b1d71d6
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-07 10:21:24 -0500

    Modified paginator function

--- assets/js/jquery.paginate.js
+++ assets/js/jquery.paginate.js
@@ -1,34 +1,3 @@
-/**
- * Paginate - jQuery Plugin
- * By Federico Cargnelutti <fedecarg@gmail.com>
- * 
- * Dual licensed under the MIT and GPL licenses:
- *   - http://www.opensource.org/licenses/mit-license.php
- *   - http://www.gnu.org/licenses/gpl.html
- * 
- * Examples and documentation at: 
- *   - http://github.com/fedecarg/jquery-htmlpaginate
- * 
- * Usage:
- * 
- * <ul id="items">
- *     <li>Item 1</li>
- *     <li>Item 2</li>
- *     <li>Item 3</li>
- *     <li>Item 4</li>
- *     <li>Item 5</li>
- *     <li>Item 6</li>
- * </ul>
- * <div id="items-pagination" style="display:none">
- *     <a id="items-previous" href="#">&laquo; Previous</a> 
- *     <a id="items-next" href="#">Next &raquo;</a>
- * </div>
- *     
- * <script type="text/javascript">
- * $('#items').paginate({itemsPerPage: 2});
- * </script>
- * 
- */
 (function($) {
     
 $.fn.paginate = function(options) {
@@ -54,7 +23,7 @@ $.fn.paginate = function(options) {
         var init = function() {
             numberOfItems = self.children().size();
             numberOfPages = Math.ceil(numberOfItems / options.itemsPerPage);
-            if (numberOfPages > 1) {
+            if (numberOfPages >= 1) {
                 $(options.selector.pagination).show();
                 $(options.selector.previous).addClass(options.cssClassName.disabled);
             }
@@ -76,7 +45,7 @@ $.fn.paginate = function(options) {
         
         var show = function(page) {
             currentPage = page;
-            startPage = (currentPage - 1) * options.itemsPerPage;
+            startPage = (currentPage) * options.itemsPerPage;
             endPage = startPage + options.itemsPerPage;
             self.children().hide().slice(startPage, endPage).show();
 
