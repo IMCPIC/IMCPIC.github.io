8442e8d2939406a055aff7f7bc45f0e39a2253c4
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-07 13:49:31 -0500

    closed h4 tags from h2

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -31,8 +31,8 @@ $('#search2').keyup(function () {
       console.log(val);
       found = 1;
       output += '<li>';
-      output += '<h4>' + val.date + '</h2>';
-      output += '<h4>' + val.title + '</h2>';
+      output += '<h4>' + val.date + '</h4>';
+      output += '<h4>' + val.title + '</h4>';
       output += '</li>';
       } 
    });
