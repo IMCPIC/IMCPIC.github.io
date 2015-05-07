f579afda2b8814f83fc28aec3041aec3b4c44879
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-07 17:31:57 -0500

    BLOG LAB COMPLETE

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -15,19 +15,81 @@ function getposts(){
       	});
     });
 }
-//
+
+
+
 $(document).ready(function() {
             
-            //set focus to 1st input field
             $("search2").focus();
             
-            //attach click event to button
             $("#search2").keydown(function(){
                 $("#search1").focus();
             });
             
 
         });
+
+
+
+
+$('#search1').keyup(function () { 
+    var searchField = $('#search1').val();
+    if (searchField.length){        
+    var myExp = new RegExp(searchField, "i");
+    var found = 0;
+ 
+   $.getJSON('entries.json', function (data) {
+    var output = '<ul class="searchresults" id="searchresults">';
+ 	   
+   $.each(data.entries.reverse(), function(key, val) {
+      if (val.text.search(myExp) !== -1 || val.title.search(myExp) !== -1) {
+      console.log(val);
+      found = 1;
+      output += '<br>';
+      output += '<li>';
+      output += '<h5>' + val.date + '</h5>';
+      output += '<h4>' + val.title + '</h4>';
+      output += '<p>' + val.text + '</p>';
+      output += '</li>';
+      }
+   });
+      output += '</ul>';
+
+      output += '<div id="searchresults-pagination" class="update" style="float:right;">';
+	  output += '<a id="searchresults-previous" href="#">&laquo; Newer</a>';
+	  output += ' | ';
+	  output += '<a id="searchresults-next" href="#">Older &raquo;</a>';
+      output += '</div>';
+      output += '<div class="col-sm-12" style="float:right;">';
+      output += '<p></hr></p>';
+      output += '</div>'
+
+      if (found==1) {
+      $('#update').removeClass('update-hidden');
+      $('#searchhide').addClass('searchhide-hidden');
+      $('#searchhide2').addClass('searchhide-hidden');
+      $('#update').html(output);
+      $('#searchresults').paginate({itemsPerPage: 4});
+      }
+      else {
+          $('#update').addClass('update-hidden');
+          $('#searchhide').removeClass('searchhide-hidden');
+          $('#searchhide2').removeClass('searchhide-hidden');
+      }
+ 	 
+});
+    } else {
+      $('#update').addClass('update-hidden');
+      $('#searchhide').removeClass('searchhide-hidden');
+      $('#searchhide2').removeClass('searchhide-hidden');
+    }
+});
+
+
+
+
+
+
 
 /*$('#search2').keydown(function () {
     $("#search2").blur(function(e){
@@ -89,56 +151,3 @@ $(document).ready(function() {
 });*/
 
 
-
-$('#search1').keyup(function () { 
-    var searchField = $('#search1').val();
-    if (searchField.length){        
-    var myExp = new RegExp(searchField, "i");
-    var found = 0;
- 
-   $.getJSON('entries.json', function (data) {
-    var output = '<ul class="searchresults" id="searchresults">';
- 	   
-   $.each(data.entries.reverse(), function(key, val) {
-      if (val.text.search(myExp) !== -1 || val.title.search(myExp) !== -1) {
-      console.log(val);
-      found = 1;
-      output += '<br>';
-      output += '<li>';
-      output += '<h5>' + val.date + '</h5>';
-      output += '<h4>' + val.title + '</h4>';
-      output += '<p>' + val.text + '</p>';
-      output += '</li>';
-      }
-   });
-      output += '</ul>';
-
-      output += '<div id="searchresults-pagination" class="update" style="float:right;">';
-	  output += '<a id="searchresults-previous" href="#">&laquo; Newer</a>';
-	  output += ' | ';
-	  output += '<a id="searchresults-next" href="#">Older &raquo;</a>';
-      output += '</div>';
-      output += '<div class="col-sm-12" style="float:right;">';
-      output += '<p></hr></p>';
-      output += '</div>'
-
-      if (found==1) {
-      $('#update').removeClass('update-hidden');
-      $('#searchhide').addClass('searchhide-hidden');
-      $('#searchhide2').addClass('searchhide-hidden');
-      $('#update').html(output);
-      $('#searchresults').paginate({itemsPerPage: 4});
-      }
-      else {
-          $('#update').addClass('update-hidden');
-          $('#searchhide').removeClass('searchhide-hidden');
-          $('#searchhide2').removeClass('searchhide-hidden');
-      }
- 	 
-});
-    } else {
-      $('#update').addClass('update-hidden');
-      $('#searchhide').removeClass('searchhide-hidden');
-      $('#searchhide2').removeClass('searchhide-hidden');
-    }
-});
