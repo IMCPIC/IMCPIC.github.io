b6a4a50f297c1b4077759e3457a3dc8ae07693c1
unknown <ERichter_H@Dev02.bsecode.inc>
2015-05-08 17:25:38 -0500

    Changed blog post title size

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -5,7 +5,7 @@ function getposts(){
 		$.each(data.entries.reverse(), function(key, val) {
         var output="<div id='key' class='col-sm-12 blogpost'>";
 		    output+="<small><p class='muted' style='float:right;'>" + val.date +"</p></small>";
-		    output+="<h5>" + val.title +"</h5>";
+		    output+="<h4>" + val.title +"</h4>";
 		    output+="<p>" + val.text +"</p>";
 		    output+="</hr>";
 			  output+="</div>";
