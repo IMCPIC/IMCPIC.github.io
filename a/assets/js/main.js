e009a96b7caf1c4ea368d606ba5e77926bed2e89
Erik Richter <ERichter_H@Dev02.bsecode.inc>
2015-05-12 15:16:58 -0500

    Removed title with just link

--- a/assets/js/main.js
+++ a/assets/js/main.js
@@ -6,8 +6,7 @@ function getstories(){
     $.getJSON('stories.json', function(data) {
 		$.each(data.list, function(key, val) {
         var output="<div id='stories' class='col-sm-12 blogpost'>";
-		    output+="<h3>" + val.resolved_title +"</h3>";
-		    output+="<a href='val.resolved_url'>"+"<h4>"+val.resolved_title+"</h4>"+"</a>";
+		    output+="<a href='val.resolved_url'>"+"<h3>"+val.resolved_title+"</h3>"+"</a>";
 		    output+="<p>" + val.excerpt +"</p>";
 		    output+="</hr>";
 			output+="</div>";
