6945219bb73d7189c5520bf157b8622f8e25bb6c
Erik Richter <ERichter_H@Dev02.bsecode.inc>
2015-05-12 14:32:33 -0500

    Added reverse back to JS

--- a/assets/js/main.js
+++ a/assets/js/main.js
@@ -4,12 +4,11 @@ $(document).ready(getstories);
 function getstories(){
 	
     $.getJSON('stories.json', function(data) {
-		$.each(data.list, function(key, val) {
+		$.each(data.list.reverse(), function(key, val) {
         var output="<div id='stories' class='col-sm-12 blogpost'>";
 		    output+="<h3>" + val.resolved_title +"</h3>";
 		    output+="<h4>" + val.resolved_url +"</h4>";
 		    output+="<p>" + val.excerpt +"</p>";
-		    output+="<p>" + val +"</p>";
 		    output+="</hr>";
 			output+="</div>";
 
