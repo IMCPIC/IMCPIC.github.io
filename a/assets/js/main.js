afb11067cdaa502ece5687b4a5378aabdcca7441
Erik Richter <ERichter_H@Dev02.bsecode.inc>
2015-05-12 15:22:47 -0500

    Updated URL link

--- a/assets/js/main.js
+++ a/assets/js/main.js
@@ -6,7 +6,7 @@ function getstories(){
     $.getJSON('stories.json', function(data) {
 		$.each(data.list, function(key, val) {
         var output="<div id='stories' class='col-sm-12 blogpost'>";
-		    output+="<a href='val.resolved_url'>"+"<h4>"+val.resolved_title+"</h4>"+"</a>";
+		    output+="<a href="+val.resolved_url+">"+"<h4>"+val.resolved_title+"</h4>"+"</a>";
 		    output+="<p>" + val.excerpt +"</p>";
 		    output+="</hr>";
 			output+="</div>";
